b739c8e184a76919a94677d8521af1be1db402d3
efoosag <96612103+efoosag@users.noreply.github.com>
2022-05-09 13:52:38 +0100

    change double quote to single quote

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-const hamburger = document.querySelector(".hamburger");
-const Menu = document.querySelector(".menu");
+const hamburger = document.querySelector('.hamburger');
+const Menu = document.querySelector('.menu');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  Menu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  Menu.classList.toggle('active');
 });
 
-document.querySelectorAll(".menu-item").forEach((n) => {
-  n.addEventListener("click", () => {
-    Menu.classList.toggle("active");
-    hamburger.classList.toggle("active");
+document.querySelectorAll('.menu-item').forEach((n) => {
+  n.addEventListener('click', () => {
+    Menu.classList.toggle('active');
+    hamburger.classList.toggle('active');
   });
 });
