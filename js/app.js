b37c77eda6d7c5dbf1a09dc4a25fa44310beb8f6
efoosag <96612103+efoosag@users.noreply.github.com>
2022-05-12 14:40:20 +0100

    update for local storage

--- js/app.js
+++ js/app.js
@@ -213,3 +213,19 @@ function inputData() {
   };
   localStorage.setItem('contactInfo', JSON.stringify(contact));
 }
+const retrieveData = localStorage.getItem('contactInfo');
+if (retrieveData) {
+  document.querySelector('.name').value = JSON.parse(retrieveData).name;
+  document.querySelector('.email').value = JSON.parse(retrieveData).email;
+  document.querySelector('.msg').value = JSON.parse(retrieveData).msg;
+} else {
+  document.getElementById('name').onchange = () => {
+    inputData();
+  };
+  document.getElementById('email').onchange = () => {
+    inputData();
+  };
+  document.getElementById('msg').onchange = () => {
+    inputData();
+  };
+}
