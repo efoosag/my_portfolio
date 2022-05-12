9d632eb2e879ffcb355e6f8e0971c26a324b3972
efoosag <96612103+efoosag@users.noreply.github.com>
2022-05-12 14:45:02 +0100

    move local storage code to different file

--- js/app.js
+++ js/app.js
@@ -204,28 +204,3 @@ form.addEventListener('click', (e) => {
   e.preventDefault();
   validateEmail(mail);
 });
-let contact = {};
-function inputData() {
-  contact = {
-    name: document.querySelector('.name').value,
-    email: document.querySelector('.email').value,
-    msg: document.querySelector('.msg').value,
-  };
-  localStorage.setItem('contactInfo', JSON.stringify(contact));
-}
-const retrieveData = localStorage.getItem('contactInfo');
-if (retrieveData) {
-  document.querySelector('.name').value = JSON.parse(retrieveData).name;
-  document.querySelector('.email').value = JSON.parse(retrieveData).email;
-  document.querySelector('.msg').value = JSON.parse(retrieveData).msg;
-} else {
-  document.getElementById('name').onchange = () => {
-    inputData();
-  };
-  document.getElementById('email').onchange = () => {
-    inputData();
-  };
-  document.getElementById('msg').onchange = () => {
-    inputData();
-  };
-}
